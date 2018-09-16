39e9caccf3e1489f4f3a586316c1cf0860fb9a9f
V10LET <violet.wmoon@gmail.com>
2018-09-16 16:53:04 -0500

    learned how to create new routes for a GET request

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,44 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+# ------> Items GET request
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
+# ------> Search GET request
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+# ------> Add item to cart GET request
+    elsif req.path.match(/add/)
+      get = req.params["item"]
+      if @@items.include?(get)
+        @@cart << get
+        resp.write "added #{get}"
+      else
+        resp.write "We don't have that item"
+      end
+
+# ------> Display cart GET request
+    elsif req.path.match(/cart/)
+      if @@cart == []
+        resp.write "Your cart is empty"
+      else
+        @@cart.each { |item|
+            resp.write "#{item}\n"
+        }
+      end
+
+# ------> Display invalid GET request
     else
       resp.write "Path Not Found"
     end
